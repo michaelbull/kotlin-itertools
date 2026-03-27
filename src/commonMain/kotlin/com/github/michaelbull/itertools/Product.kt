68fcd93665dfa71521873f0b0db0fff8ff25bf31
Michael Bull <mikebull94@gmail.com>
2026-03-27 04:23:33 +0000

    Add equivalent code example for Python's product repeat parameter

--- src/commonMain/kotlin/com/github/michaelbull/itertools/Product.kt
+++ src/commonMain/kotlin/com/github/michaelbull/itertools/Product.kt
@@ -137,8 +137,13 @@ public fun <A, B, C> Triple<Iterable<A>, Iterable<B>, Iterable<C>>.product(): Se
  * - Ruby [Array#product](https://ruby-doc.org/3.3.0/Array.html#method-i-product)
  * - Rust [Itertools::cartesian_product](https://docs.rs/itertools/latest/itertools/trait.Itertools.html#method.cartesian_product)
  *
- * Unlike Python's `itertools.product(iterable, repeat=n)`, this function does not accept a `repeat` parameter. The
- * Cartesian product of a list with itself is equivalent to [permutationsWithReplacement].
+ * Unlike Python's `itertools.product(iterable, repeat=n)`, this function does not accept a `repeat` parameter.
+ * The Cartesian product of a list with itself is equivalent to [permutationsWithReplacement]:
+ *
+ * ```
+ * // Python: itertools.product("ABCD", repeat=2)
+ * "ABCD".toList().permutationsWithReplacement(k = 2)
+ * ```
  */
 public fun <T> List<List<T>>.product(): Sequence<List<T>> {
     return if (isEmpty()) {
